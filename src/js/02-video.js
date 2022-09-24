da8d56943ce3f7c427f67d9f298c5b03050b5b7f
Olga Birkovska <birkovskaya92@ukr.net>
2022-09-24 21:02:33 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -7,7 +7,8 @@ console.log(player);
 
     player.on('timeupdate', function() {
         console.log('time updated');
-        const currentTime = (player.getCurrentTime());
+        const currentTime = (player.getCurrentTime().then(function(seconds) {}));
+        console.log(currentTime);
         localStorage.setItem('videoplayer-current-time', JSON.stringify(currentTime));
     });
 
