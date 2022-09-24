728679f4bbfee139e478901c808872e468842a0b
Olga Birkovska <birkovskaya92@ukr.net>
2022-09-24 20:40:14 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+console.log(player);
+
+    player.on('timeupdate', function() {
+        console.log('time updated');
+        localStorage.setItem('videoplayer-current-time', currentTime);
+    });
+
+    // player.getVideoTitle().then(function(title) {
+    //     console.log('title:', title);
+    // });
+
+    player.setCurrentTime(localStorage.getItem('videoplayer-current-time')).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+    });
