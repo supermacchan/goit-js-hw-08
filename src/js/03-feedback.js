614919cd149265e635d6eb9ec01ba1a209426004
Olga Birkovska <birkovskaya92@ukr.net>
2022-09-24 22:27:59 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,23 @@
+import throttle from 'lodash.throttle';
+
+const emailInput = document.querySelector('input');
+const messageInput = document.querySelector('textarea');
+const user = {
+    email: '',
+    message: '',
+}
+
+emailInput.addEventListener('input', throttle(onEmailInput, 500));
+messageInput.addEventListener('input', throttle(onMessageInput, 500));
+
+function onEmailInput(e) {
+    user.email = e.target.value;
+    localStorage.setItem('feedback-form-state', JSON.stringify(user));
+}
+
+function onMessageInput(e) {
+    user.message = e.target.value;
+    localStorage.setItem('feedback-form-state', JSON.stringify(user));
+}
+
 
